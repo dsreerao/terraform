95c133007345cbbb847925c780a4b2f18357f331
chinnu <rdevisreerao@gmail.com>
2025-05-22 11:00:13 +0530

    just added tag

--- statefile/main.tf
+++ statefile/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "name" {
     ami = "ami-0953476d60561c955"
     instance_type = "t2.micro"
     key_name = "tfk"
-    availability_zone = "us-east-1b"  
+    availability_zone = "us-east-1b" 
+    tags = {
+        Name = "nametry"
+    }
 }
 
