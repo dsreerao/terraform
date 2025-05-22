a58b8085521b1a26eb74703fb368349b9c1fb04a
chinnu <rdevisreerao@gmail.com>
2025-05-22 14:46:42 +0530

    changed az

--- statefile/main.tf
+++ statefile/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
     ami = "ami-0953476d60561c955"
     instance_type = "t2.micro"
     key_name = "tfk"
-    availability_zone = "us-east-1b" 
+    availability_zone = "us-east-1a" 
     tags = {
         Name = "newone"
     }
